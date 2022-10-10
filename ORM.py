38832ac2f63539747cd4ee991d3e0f421dce37f3
Bledger1 <95543134+Bledger1@users.noreply.github.com>
2022-10-10 17:14:36 +0300

    Update ORM.py

--- ORM.py
+++ ORM.py
@@ -1,4 +1,28 @@
-import sqlite3
+from peewee import *
 
-db = sqlite3.connect("root.sqlite")
-cursor = db.cursor()
+db = SqliteDatabase("root.db")
+
+class BaseModel(Model):
+
+    class Meta:
+        database = db
+
+class User(BaseModel):
+    vk_id = PrimaryKeyField(), #vk айди
+    settlement = TextField(), #название поселение
+    clan = TextField(null=True), #клан значение может быть пустым
+    balance = AutoField(), #казна
+    eat = AutoField() #еда
+    residents = AutoField() #жители
+    farmers = AutoField() #фермеры
+    warrios = AutoField() #воины
+    income = AutoField() #доход
+    collecting = AutoField() #сбор
+    сonsumption = AutoField() #потребление
+    headquarters =AutoField() #штаб
+    village = AutoField()#деревня
+    garden = AutoField() #сад
+    army = AutoField() #армия
+
+if __name__ == '__main__':
+    db.create_tables([User])
